4f97cc4031556e70afc390a3fe6ae9bd2bdb5bc3
JeffBship <jeffrey.blankenship@my.ccsu.edu>
2017-04-18 08:25:56 -0400

    Added second js file

---
+++ CeasarTool/public_html/javascript2.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//var freqOrder   = {'e','t','a','o','i','n','s','h','r','d','l','c','u',
+//                   'm','w','f','g','y','p','b','v','k','j','x','q','z'};
+//var freqPercent = {12.702,9.056,8.167,7.507,6.966,6.749,6.327,6.094,5.987,4.253,4.025,2.782,2.758,
+//                  2.406,2.360,2.228,2.015,1.974,1.929,1.492,0.978,0.772,0.153,0.150,0.095,0.074};
+                 
+function spellcheck(){
+  
+  
+  }
