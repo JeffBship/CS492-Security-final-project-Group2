dafe1b905fb269ec699d7e3983ffe1946840d332
Jeff Blankenship <jeffrey.blankenship@my.ccsu.edu>
2017-04-24 09:24:03 -0400

    fixed layout

--- CeasarTool/public_html/javascript.js
+++ CeasarTool/public_html/javascript.js
@@ -1,7 +1,7 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Adrian Ward-Manthey and Jeff Blankenship
+ * CS492 Final Project.
+ * Prof Williams
  */
 
 
